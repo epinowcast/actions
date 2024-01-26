8ae42fda6d5de4ee6857f61aed5707273160e3e7
Sam Abbott <contact@samabbott.co.uk>
2024-01-26 22:54:46 +0000

    Create index.js

---
+++ install-cmdstan/index.js
@@ -0,0 +1,39 @@
+const core = require('@actions/core');
+const cache = require('@actions/cache');
+const exec = require('@actions/exec');
+const axios = require('axios');
+
+async function run() {
+  try {
+    let cmdstanVersion = core.getInput('cmdstan-version');
+
+    if (cmdstanVersion === 'latest') {
+      const response = await axios.get('https://api.github.com/repos/stan-dev/cmdstan/releases/latest');
+      cmdstanVersion = response.data.tarball_url.split('/').pop().split('v').pop();
+    }
+
+    const cmdstanPath = process.env['HOME'] + '/.cmdstan';
+    const cacheKey = `cmdstan-${cmdstanVersion}`;
+
+    let cacheHit = await cache.restoreCache([cmdstanPath], cacheKey);
+
+    if (!cacheHit) {
+      await exec.exec(`bash ${__dirname}/install_cmdstan_script.sh`, [cmdstanVersion, cmdstanPath]);
+
+      try {
+        await cache.saveCache([cmdstanPath], cacheKey);
+      } catch (error) {
+        if (error.name !== cache.ValidationError.name && error.name !== cache.ReserveCacheError.name) {
+          core.info(`[warning] ${error.message}`);
+        }
+      }
+    } else {
+      core.info(`Using cached CmdStan version ${cmdstanVersion}`);
+    }
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
