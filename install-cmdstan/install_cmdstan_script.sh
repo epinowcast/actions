a01ecf722727de0bceddb544aebf5532d4a2f2a2
Sam Abbott <contact@samabbott.co.uk>
2024-01-26 22:57:12 +0000

    Create install_cmdstan_script.sh

---
+++ install-cmdstan/install_cmdstan_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+version=$1
+cmdstan_path=$2
+
+# Install CmdStan
+mkdir -p "${cmdstan_path}"
+cd "${cmdstan_path}" || exit
+wget "https://github.com/stan-dev/cmdstan/releases/download/v${version}/cmdstan-${version}.tar.gz"
+tar -xzf "cmdstan-${version}.tar.gz"
+cd "cmdstan-${version}" || exit
+make build
