27b37266a7c0b7b184397db20663055d744ddefd
Sam Abbott <contact@samabbott.co.uk>
2024-01-26 23:26:31 +0000

    Create install_cmdstan_win.ps1

---
+++ install-cmdstan/install_cmdstan_win.ps1
@@ -0,0 +1,34 @@
+param (
+    [string]$version,
+    [string]$cmdstanPath,
+    [int]$numCores = 1
+)
+
+function Install-CmdStan {
+    $cmdstanUrl = "https://github.com/stan-dev/cmdstan/releases/download/v$version/cmdstan-$version.tar.gz"
+    $cmdstanTarGz = "cmdstan-$version.tar.gz"
+    $cmdstanDir = "cmdstan-$version"
+
+    # Create directory if it does not exist
+    if (-not (Test-Path $cmdstanPath)) {
+        New-Item -ItemType Directory -Force -Path $cmdstanPath
+    }
+
+    # Change to the CmdStan path
+    Set-Location -Path $cmdstanPath
+
+    # Download CmdStan
+    Invoke-WebRequest -Uri $cmdstanUrl -OutFile $cmdstanTarGz
+
+    # Extract the tar.gz file
+    & tar -xzf $cmdstanTarGz
+
+    # Change to the CmdStan directory
+    Set-Location -Path $cmdstanDir
+
+    # Build CmdStan
+    & make -j$numCores build
+}
+
+# Run the install function
+Install-CmdStan
