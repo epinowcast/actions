37ac039185acd649e7ad8b94189f0bcd521d141c
Sam Abbott <contact@samabbott.co.uk>
2024-01-27 15:09:29 +0000

    Update install_cmdstan.sh

--- install-cmdstan/scripts/install_cmdstan.sh
+++ install-cmdstan/scripts/install_cmdstan.sh
@@ -5,8 +5,8 @@ num_cores=$2
 
 
 # Install CmdStan
-mkdir -p "cmdstan"
-cd "cmdstan" || exit
+mkdir -p ".cmdstan"
+cd ".cmdstan" || exit
 wget "https://github.com/stan-dev/cmdstan/releases/download/v${version}/cmdstan-${version}.tar.gz"
 tar -xzf "cmdstan-${version}.tar.gz"
 cd "cmdstan-${version}" || exit
