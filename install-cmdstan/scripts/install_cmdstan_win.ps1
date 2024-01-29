07d88c3268dfc009e82c47bde7eaad798aa8f133
Sam Abbott <contact@samabbott.co.uk>
2024-01-29 23:50:51 +0000

    Update install_cmdstan_win.ps1

--- install-cmdstan/scripts/install_cmdstan_win.ps1
+++ install-cmdstan/scripts/install_cmdstan_win.ps1
@@ -3,32 +3,27 @@ param (
     [int]$numCores = 1
 )
 
-function Install-CmdStan {
-    $cmdstanPath = ".cmdstan"
-    $cmdstanUrl = "https://github.com/stan-dev/cmdstan/releases/download/v$version/cmdstan-$version.tar.gz"
-    $cmdstanTarGz = "cmdstan-$version.tar.gz"
-    $cmdstanDir = "cmdstan-$version"
+# Define CmdStan directory path
+$cmdstanDir = Join-Path $env:USERPROFILE ".cmdstan\cmdstan-$version"
 
-    # Create directory if it does not exist
-    if (-not (Test-Path $cmdstanPath)) {
-        New-Item -ItemType Directory -Force -Path $cmdstanPath
-    }
-
-    # Change to the CmdStan path
-    Set-Location -Path $cmdstanPath
-
-    # Download CmdStan
-    Invoke-WebRequest -Uri $cmdstanUrl -OutFile $cmdstanTarGz
-
-    # Extract the tar.gz file
-    & tar -xzf $cmdstanTarGz
-
-    # Change to the CmdStan directory
-    Set-Location -Path $cmdstanDir
-
-    # Build CmdStan
-    & make -j$numCores build
+# Create directory if it does not exist
+if (-not (Test-Path $cmdstanDir)) {
+    New-Item -ItemType Directory -Force -Path $cmdstanDir
 }
 
-# Run the install function
-Install-CmdStan
+# Change to the CmdStan path
+Set-Location -Path $cmdstanDir
+
+# Download CmdStan
+$cmdstanUrl = "https://github.com/stan-dev/cmdstan/releases/download/v$version/cmdstan-$version.tar.gz"
+$cmdstanTarGz = "cmdstan-$version.tar.gz"
+Invoke-WebRequest -Uri $cmdstanUrl -OutFile $cmdstanTarGz
+
+# Extract the tar.gz file
+& tar -xzf $cmdstanTarGz
+
+# Change to the CmdStan directory
+Set-Location -Path "cmdstan-$version"
+
+# Build CmdStan
+& make -j$numCores build
